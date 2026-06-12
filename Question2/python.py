6df2f1efda1346ba5498d49ee72d5d2a2ee00e82
Itsme kartik.git <itsmekartii.dev@gmail.com>
2026-06-12 23:26:27 +0530

    Add submit todo API and MongoDB storage

--- Question2/python.py
+++ Question2/python.py
@@ -3,17 +3,21 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-
 client = MongoClient(
     "mongodb+srv://kartik:kartik123@flask.0wpqcoq.mongodb.net/?appName=flask"
 )
 
 db = client["testdb"]
+
 users = db["users"]
+todos = db["todos"]
+
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -33,6 +37,30 @@ def submit():
 @app.route("/success")
 def success():
     return render_template("success.html")
+
+
+@app.route("/todo")
+def todo():
+    return render_template("todo.html")
+
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo():
+    try:
+        data = {
+            "itemName": request.form["itemName"],
+            "itemDescription": request.form["itemDescription"]
+        }
+
+        todos.insert_one(data)
+
+        return """
+        <h2>Todo Item Saved Successfully!</h2>
+        <a href="/todo">Add Another Todo</a>
+        """
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
